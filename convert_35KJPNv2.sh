0dee36619c9d8717c97890106be377108c80d018
makohda <ma.kohda@gmail.com>
2018-06-27 15:35:01 +0900

    renamed: 00_convert_35KJPNv2.sh, 10_tommo_separate_alternatives.rb

--- convert_35KJPNv2.sh
+++ convert_35KJPNv2.sh
@@ -5,7 +5,7 @@ echo "create tommo-3.5kjpnv2-20180625-af_snvall.MAF.genericdb"
 foreach vcf in data/tommo-3.5kjpnv2-20180625-af_snvall-{autosome,chrX_PAR2,chrX_PAR3,chrMT}.vcf
   perl -F"\t" -lane '$F[7] =~ s/.*?(AF=0.\d+|AF=0.\d+,0.\d+|AF=0.\d+,0.\d+,0.\d+|AF=1);.*/$1/; print join("\t", @F[0,1,1,3,4], $F[7])' ${vcf}
 end | grep -v "^#" > tommo-3.5kjpnv2-20180625-af_snvall.MAF.genericdb.org
-./10_tommo_separate_alternatives.rb tommo-3.5kjpnv2-20180625-af_snvall.MAF.genericdb.org | perl -pe 's/AF=//' > tommo-3.5kjpnv2-20180625-af_snvall.MAF.genericdb
+./tommo_separate_alternatives.rb tommo-3.5kjpnv2-20180625-af_snvall.MAF.genericdb.org | perl -pe 's/AF=//' > tommo-3.5kjpnv2-20180625-af_snvall.MAF.genericdb
 echo "complete"
 
 # other INFO
@@ -18,7 +18,7 @@ foreach vcf in data/{tommo-3.5kjpnv2-20180625-af_snvall-chrX_PAR2.vcf,tommo-3.5k
   perl -F"\t" -lane '$F[7] =~ s/AC=.*?;AN=\d+;AF=.*?;(.*)/$1/; $F[7] =~ s/(AF_MALE|AF_FEMALE)=.*?;//g; $F[7] =~ s/;ANN=.*//; $F[7] =~ s/AC_FEMALE/ AC_FEMALE/; print join("\t", @F[0,1,1,3,4], $F[7])' ${vcf}
 end | grep -v "^#" >> tommo-3.5kjpnv2-20180625-af_snvall.INFO.genericdb.org
 
-./10_tommo_separate_alternatives.rb tommo-3.5kjpnv2-20180625-af_snvall.INFO.genericdb.org > tommo-3.5kjpnv2-20180625-af_snvall.INFO.genericdb
+./tommo_separate_alternatives.rb tommo-3.5kjpnv2-20180625-af_snvall.INFO.genericdb.org > tommo-3.5kjpnv2-20180625-af_snvall.INFO.genericdb
 echo "complete"
 
 echo "check line numbers in two files"
